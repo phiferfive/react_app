f83d211adf2ee8668166bee0e4702418b0e050bc
Lance Watanabe <phiferthree@gmail.com>
2021-12-05 17:33:35 -0800

    no message

--- src/Customers.js
+++ src/Customers.js
@@ -41,7 +41,7 @@ export default class Customers extends Component {
               <p>{customer.phone}</p>
               <Button bsStyle="info" onClick={() => this.setState({selectedCustomer: customer.id})}>
 
-                Click to View Details
+                Click to View Details2
 
               </Button>
 
